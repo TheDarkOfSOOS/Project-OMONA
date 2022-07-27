2bd910be057d6b6833d5b2523e89fff8f636750b
TheDarkOfSOOS <marconcini.raul@gmail.com>
2022-07-27 14:47:50 +0200

    aggiunta animazione saetta

--- damage_formula.py
+++
@@ -1,8 +0,0 @@
-import random as rand
-
-# FUNZIONE CHE SERVE PER CALCOLARE IL DANNO
-
-# [ attacco * (potenza attacco * 0.3) ] - [ difesa del nemico + (valore tra 1 a 7) ]
-
-def damage_formula(atk,skill_atk,defn):
-    return [atk * (skill_atk * 0.3)] - [defn + rand.randrange(1,8)]
